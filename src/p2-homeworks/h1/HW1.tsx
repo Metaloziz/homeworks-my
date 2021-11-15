622664236a24f3267eb17fa12226b77517016034
Metaloziz <tesla9760614>
2021-11-15 11:11:55 +0300

    Install

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Message} from "./Message";
+
+const messageData = {
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6z5Asj733AuRYdGiVV7sBn_WqNEDLw6E3Tg&usqp=CAU',
+    name: 'Child of Korn',
+    message: 'Hello',
+    time: "22.01",
+}
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            {/*should work (должно работать)*/}
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import style from './Message.module.css'
+
+type messageDataPropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+export const Message = (props: messageDataPropsType) => {
+    return (
+        <div className={style.post}>
+            <div className={style.ava}>
+
+                <div className={style.back}>
+                    <img alt={"ava"} src={props.avatar}/>
+                </div>
+            </div>
+
+            <div className={style.block}>
+                <div className={style.name}>{props.name}</div>
+                <div className={style.message}>{props.message}</div>
+                <div className={style.time}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+
