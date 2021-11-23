f1a597fe63bc176e92206b21439eda431f5e8363
Metaloziz <tesla9760614>
2021-11-23 17:48:40 +0300

    test

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -4,7 +4,7 @@ import {Message} from "./Message";
 const messageData = {
     avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6z5Asj733AuRYdGiVV7sBn_WqNEDLw6E3Tg&usqp=CAU',
     name: 'Child of Korn',
-    message: 'Hello',
+    message: 'Hello my friend',
     time: "22.01",
 }
 
