8ad08d9a021c218569fbda70ced6ce04338ca23b
Metaloziz <tesla9760614>
2021-11-26 12:49:53 +0300

    refactoring

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import {Affairs} from './Affairs'
 
-
 export type AffairPriorityType = Array<AffairType>
 export type AffairType = {
     _id: number
@@ -9,7 +8,6 @@ export type AffairType = {
     priority: FilterType
 }
 export type FilterType = 'all' | 'high' | 'low' | 'middle'
-
 
 const defaultAffairs: AffairPriorityType = [
     {_id: 1, name: 'React', priority: 'high'},
@@ -19,24 +17,21 @@ const defaultAffairs: AffairPriorityType = [
     {_id: 5, name: 'html & css', priority: 'middle'},
 ]
 
-
 export const filterAffairs = (affairs: AffairPriorityType, filter: FilterType): AffairPriorityType => {
-    if (filter === 'all') {return affairs}
-    else
-
-        return affairs.filter( item => item.priority === filter)
+    if (filter === 'all') {
+        return affairs
+    } else return affairs.filter(item => item.priority === filter)
 }
+
 export const deleteAffair = (affairs: AffairPriorityType, _id: number): AffairPriorityType => {
-    return affairs.filter( item => item._id !== _id)
+    return affairs.filter(item => item._id !== _id)
 }
 
 function HW2() {
     const [affairs, setAffairs] = useState<AffairPriorityType>(defaultAffairs)
     const [filter, setFilter] = useState<FilterType>('all')
-
     const filteredAffairs = filterAffairs(affairs, filter)
     const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id))
-
     return (
         <div>
             homeworks 2
