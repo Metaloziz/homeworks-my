06f87f1160df795230217ae7cac5b1d9f002a50f
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-03 16:31:49 +0300

    11 task done with strange css

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,44 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import {SuperDoubleRange} from './common/c8-SuperDoubleRange/SuperDoubleRange'
 import s from './HW11.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onChangeRange = (value: [number, number]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
     return (
         <div>
             <hr/>
             <span className={s.title}>homeworks 11</span>
-
-            {/*should work (должно работать)*/}
             <div className={s.body}>
-                <div>
+                <div className={s.firstRange}>
                     <span>{value1}</span>
-                    <SuperRange value={value1} onChangeRange={setValue1}
-                        // сделать так чтоб value1 изменялось
-                    />
+                    <SuperRange value={value1} onChangeRange={setValue1}/>
                 </div>
-
-                <div>
-                    <span>{value1}</span>
-                    <SuperDoubleRange value={[value1, value2]}
-                        // сделать так чтоб value1 и value2 изменялось
-                    />
-                    <div><span>{value2}</span></div>
+                <div className={s.secondRange}>
+                    <div>
+                        <span>{value1}</span>
+                    </div>
+                    <div>
+                        <SuperDoubleRange
+                            onChangeRange={onChangeRange}
+                            value={[value1, value2]}
+                            min={0}
+                            max={1000}
+                            width={200}/>
+                    </div>
+                    <div>
+                        <span>{value2}</span>
+                    </div>
                 </div>
             </div>
-
             <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
             <hr/>
         </div>
     )
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,47 +1,100 @@
-import React from 'react'
-// import noUiSlider from 'nouislider';
+import React, {useEffect, useState} from 'react'
+import style from './SuperDoubleRange.module.css'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
+    onChangeRange: (value: [number, number]) => void
     value: [number, number]
-    // min, max, step, disable, ...
+    min: number
+    max: number
+    width: number
+    disable?: boolean
+    step?: number
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+export const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
+        min,
+        max,
+        width,
+        disable,
+        step = 1,
+        ...restProps
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
 
-    // let slider
-    //
-    // slider = document.getElementById('slider');
-    // if (slider) {
-    //     noUiSlider.create(slider, {
-    //         start: [20, 80],
-    //         connect: true,
-    //         range: {
-    //             'min': 0,
-    //             'max': 100
-    //         }
-    //     });
-    // }
+    const [avg, setAvg] = useState((min + max) / 2);
+    const [minVal, setMinVal] = useState(value[0]);
+    const [maxVal, setMaxVal] = useState(value[1]);
 
+
+    const minWidth = ((avg - min) / (max - min)) * (width - 2);
+    const minPercent = ((minVal - min) / (avg - min)) * 100;
+    const maxPercent = ((maxVal - avg) / (max - avg)) * 100;
+    const styles = {
+        min: {
+            width: minWidth,
+            left: 0,
+            "--minRangePercent": `${minPercent}%`
+        },
+        max: {
+            width: ((max - avg) / (max - min)) * (width - 2),
+            left: minWidth,
+            "--maxRangePercent": `${maxPercent}%`
+        }
+    };
+
+    useEffect(() => {
+        setAvg((maxVal + minVal) / 2);
+    }, [minVal, maxVal]);
 
     return (
         <>
-            {/*<div id="slider"/>*/}
-            <input
-                type={'range'}
-                min={0}
-                max={100}
-                value={value[0]}
-
-            />
+            <div
+                className={style.min_max_slider}
+                data-legendnum="2"
+                data-rangemin={min}
+                data-rangemax={max}
+                data-rangewidth={width}
+            >
+                <label htmlFor="min">Minimum price</label>
+                <input
+                    id="min"
+                    className={style.min}
+                    style={styles.min}
+                    name="min"
+                    type="range"
+                    step={step}
+                    min={min}
+                    max={avg}
+                    value={minVal}
+                    onChange={({target}) => {
+                        onChangeRange([Number(target.value), maxVal])
+                        setMinVal(Number(target.value))
+                    }}
+                    disabled={disable}
+                />
+                <label htmlFor="max">Maximum price</label>
+                <input
+                    id="max"
+                    className={style.max}
+                    style={styles.max}
+                    name="max"
+                    type="range"
+                    step={step}
+                    min={avg}
+                    max={max}
+                    value={maxVal}
+                    onChange={({target}) => {
+                        onChangeRange([minVal, Math.round(Number(target.value))])
+                        setMaxVal(Math.round(Number(target.value)))
+                    }}
+                    disabled={disable}
+                />
+            </div>
         </>
     )
 }
 
-export default SuperDoubleRange
+
+
