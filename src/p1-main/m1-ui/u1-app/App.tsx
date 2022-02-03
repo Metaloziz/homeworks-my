e7c5ffeabb8d7cd4d66ebf54001d9d002d7424db
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-03 16:44:51 +0300

    test commit without .idia

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -3,6 +3,9 @@ import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5/HW5";
 
 function App() {
+
+    // test commit without .idia
+
     return (
         <div className={s.App}>
             <div className={s.AppDiv}>react homeworks:</div>
